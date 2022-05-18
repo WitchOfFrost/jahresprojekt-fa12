34c4f994fc618d0b202eb90888086ba412416845
WitchOfFrost <github@emilia-tan.com>
2022-05-18 13:12:33 +0200

    zz

--- src/classes/gameGrid.py
+++ src/classes/gameGrid.py
@@ -2,31 +2,30 @@ from projectConfig import *
 
 from classes.gameTile import gameTile
 
-from exceptions.InvalidModeType import InvalidModeType
-
 class gameGrid():
     def __init__(self, mode):
         self.totalTiles = projectConfig["gridSize"] * projectConfig["gridSize"]
         self.grid = {}
+        self.mode = mode
         i = 0
-
+        
         while(i != self.totalTiles):
             i += 1
             self.grid[i] = gameTile(i, self.totalTiles, projectConfig["gridSize"])
             print(self.grid[i].__dict__)
 
-        # Bauernschach
-        if(mode == "BS"):
-            print("Not implemented.")
-            # TODO: Add Logic to generate Bauernschach Players and Start the game Instance
-        # Dame
-        elif(mode == "DM"):
-            print("Not implemented.")
-            # TODO: Add Logic to generate Dame Players and Start the game Instance
-        # Tic-Tac-Toe
-        elif(mode == "TTT"):
-            print("Not implemented.")
-            # TODO: Add Logic to generate Tic-Tac-Toe Players and Start the game Instance
-        else:
-            # FIXME: Will Crash without mode, pass any mode to avoid
-            raise InvalidModeType("Mode type " + mode + " is not a valid game mode!")
+        # # Bauernschach
+        # if(mode == "BS"):
+        #     print("Not implemented.")
+        #     # TODO: Add Logic to generate Bauernschach Players and Start the game Instance
+        # # Dame
+        # elif(mode == "DM"):
+        #     print("Not implemented.")
+        #     # TODO: Add Logic to generate Dame Players and Start the game Instance
+        # # Tic-Tac-Toe
+        # elif(mode == "TTT"):
+        #     print("Not implemented.")
+        #     # TODO: Add Logic to generate Tic-Tac-Toe Players and Start the game Instance
+        # else:
+        #     # FIXME: Will Crash without mode, pass any mode to avoid
+        #     raise InvalidModeType("Mode type " + mode + " is not a valid game mode!")
