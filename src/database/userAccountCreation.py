b428f822258a8abf8ac7c8111bfea8a61c7a5ec7
WitchOfFrost <github@emilia-tan.com>
2022-06-14 12:04:56 +0000

    Removed unused code

--- src/database/userAccountCreation.py
+++ src/database/userAccountCreation.py
@@ -8,10 +8,8 @@ from getpass import getpass
 
 conn = sqlite3.connect('data/db.sqlite')
 
-# UNUSED
-
-
 def selectDatabase(debug):
+    # UNUSED
     if(debug == True):
         conn = sqlite3.connect(input("Input Database Path"))
     else:
@@ -25,57 +23,59 @@ cur = conn.cursor()
 def getInput():
     username = str(input("Enter a Username > "))
     password = str(getpass("Enter a Password [HIDDEN] > "))
-    password_check = str(getpass("Enter the same Password again [HIDDEN] > "))
-    email = str(input("Enter an Email > "))
+    #password_check = str(getpass("Enter the same Password again [HIDDEN] > "))
+    #email = str(input("Enter an Email > "))
 
-    if (password != password_check):
-        print("Password does not match. Please try again!")
-        getInput()
-    else:
-        validityCheck(username, password, email)
+    validityCheck(username, password)
+
+    # if (password != password_check):
+    #    print("Password does not match. Please try again!")
+    #    getInput()
+    # else:
+    #    validityCheck(username, password, email)
 
 
-def validityCheck(username, password, email):
+def validityCheck(username, password):
     cur.execute("SELECT rowid FROM ACCOUNTS WHERE username = ?", (username,))
     data = cur.fetchone()
     if data is None:
         print('There is no entry for %s' % username)
 
-        cur.execute("SELECT rowid FROM ACCOUNTS WHERE email = ?", (email,))
-        data = cur.fetchone()
-        if data is None:
-            print('There is no entry for %s' % email)
+        #cur.execute("SELECT rowid FROM ACCOUNTS WHERE email = ?", (email,))
+        #data = cur.fetchone()
+        # if data is None:
+        #    print('There is no entry for %s' % email)
 
-            if re.search('^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$', email):
-                if (6 <= len(password) <= 32 and 6 <= len(username) <= 32):
+        #if re.search('^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$', email):
+        if (6 <= len(password) <= 16 and 6 <= len(username) <= 16):
 
-                    pwd = hashing(password)
+            pwd = hashing(password)
 
-                    if pwd == False:
-                        print("Failed to hash password, please try again!")
-                        getInput()
-                        return
-
-                    else:
-                        insertUser(username, pwd, email)
-                else:
-                    print(
-                        "Username and Password have to be between 6 and 32 Characters!")
+            if pwd == False:
+                    print("Failed to hash password, please try again!")
                     getInput()
                     return
+
             else:
-                print("Invalid Email!")
-                getInput()
-                return
+                insertUser(username, pwd)
         else:
-            print('Found username %s in Database at row %s' % (email, data[0]))
+            print(
+                    "Username and Password have to be between 6 and 16 Characters!")
+            getInput()
+            return
+        #else:
+        #    print("Invalid Email!")
+        #    getInput()
+        #    return
+        # else:
+        #    print('Found email %s in Database at row %s' % (email, data[0]))
     else:
         print('Found username %s in Database at row %s' % (username, data[0]))
 
 
-def insertUser(username, password, email):
-    cur.execute("INSERT INTO ACCOUNTS (username, password, email) VALUES (?,?,?)",
-                (username, str(password), email))
+def insertUser(username, password):
+    cur.execute("INSERT INTO ACCOUNTS (username, password) VALUES (?,?)",
+                (username, str(password)))
     conn.commit()
     print("Added User to Database")
 
