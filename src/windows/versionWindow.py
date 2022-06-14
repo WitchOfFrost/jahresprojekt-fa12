1fb5f6900ed57ae0083d041407d8d209bcb787f5
WitchOfFrost <github@emilia-tan.com>
2022-06-14 20:05:36 +0000

    Delete src/windows directory

--- src/windows/versionWindow.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-
-class versionWindow(tk.Toplevel):
-    def __init__(self, main):
-        super().__init__(main)
-        self.geometry("100x100")
-        self.title("Version")
